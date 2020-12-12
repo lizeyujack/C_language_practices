9dcd8e8b416c305bad3e00776665fd25d19a7a53
lizeyujack <53364734+lizeyujack@users.noreply.github.com>
2020-12-12 16:44:33 +1100

    const string

--- const指针.c
+++ const指针.c
@@ -3,9 +3,10 @@ int main()
 {
     int b = 30;
     int c = 0;
-    int * const pb = &b;
-    pb = &c;
-    
+    int * const pb = &b;//不能修改pb指针的信息
+    const int * pb = &b;//不能修改pb指针指向的值
+    const int const * pb = b;//同时符合上述两个条件
+    pb = 10;
     printf("*pb = %d\n",*pb);
     return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -3,8 +3,11 @@ int main()
 {
     int b = 30;
     int c = 0;
-    int * const pb = &b;
-    pb = &c;
+    // int * const pb = &b;//不能修改pb指针的信息
+    // const int * pb = &b;//不能修改pb指针指向的值
+    const int const * pb = b;
+    pb = 10;
+    
     printf("*pb = %d\n",*pb);
     return 0;
 }
